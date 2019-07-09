2915ef1000056697fb0df7c723a70e103f99fa0c
Scott Shepard <scott@shepardmail.com>
2019-07-09 17:46:22 -0500

    install the package

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+__version__ = '0.1'
+__author__ = 'Scott Shepard'
+__copyright__ = 'Copyright 2019 Scott Shepard'
+
+setup(name='Scott3',
+      version=__version__,
+      description='Connect to S3',
+      url='http://github.com/scottshepard/Scott3',
+      author=__author__,
+      author_email='risk-orig@avant.com',
+      packages=find_packages(),
+      install_requires=[
+            'pandas>=0.21.0',
+            'boto3',
+            'pyyaml'
+            ]
+      )
